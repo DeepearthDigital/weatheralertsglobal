85086a7dfda22369a16d771197877e2baacbbd59
simonvacher <simon@deepearth.digital>
2025-02-01 08:44:38 +0100

    Enhance UI functionality and adjust styles for alert zones.
    Added toggle functionality for alert zone visibility and improved map drawing tools for polygons and rectangles. Updated styles for better alignment, readability, and consistency. Refined modal text and alerts to improve user experience.

--- static/nav-side.js
+++ static/nav-side.js
@@ -1,16 +1,72 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const mobileNav = document.querySelector('.mobile-nav');
-
-  if (window.innerWidth > 768) {
-    if (!mobileNav.classList.contains('open')) {
-      mobileNav.classList.add('open');
+  // Function to make alerts visible or invisible
+  function toggleAlertVisibility(icon) {
+    const showAlertsButton = document.getElementById('showAlertsButton');
+    if (showAlertsButton.classList.contains('active')) {
+      // Show eye icon
+      icon.classList.remove('fa-eye-slash');
+      icon.classList.add('fa-eye');
+      icon.classList.add('fa-solid')
+      // Hide alerts
+      console.log("Hiding alerts");
+    } else {
+      // show eye-slash icon
+      icon.classList.remove('fa-eye');
+      icon.classList.add('fa-eye-slash');
+      icon.classList.add('fa-solid')
+      // show alerts
+      console.log("Showing alerts");
     }
   }
 
+function startRectangleDraw() {
+  console.log("startRectangleDraw()");
+  rectangleDrawer = new L.Draw.Rectangle(map);
+  currentDrawer = rectangleDrawer; // update currentDrawer
+  rectangleDrawer.enable();
+    map.on(L.Draw.Event.CREATED, function (e) {
+        let type = e.layerType;
+        let layer = e.layer;
+        
+        if (type === 'rectangle') {
+          drawnItems.addLayer(layer);
+          console.log("Finished rectangle draw", layer.getLatLngs());
+          rectangleDrawer.disable();
+          map.off(L.Draw.Event.CREATED)
+      }
+    });
+}
+	
+
+function startPolygonDraw() {
+  console.log("startPolygonDraw()");
+  polygonDrawer = new L.Draw.Polygon(map);
+  currentDrawer = polygonDrawer; // update currentDrawer
+  polygonDrawer.enable();
+    map.on(L.Draw.Event.CREATED, function (e) {
+        let type = e.layerType;
+        let layer = e.layer;
+        
+        if (type === 'polygon') {
+          drawnItems.addLayer(layer);
+          console.log("Finished polygon draw", layer.getLatLngs());
+          polygonDrawer.disable();
+          map.off(L.Draw.Event.CREATED)
+      }
+    });
+}
+
+var showHideAlertZonesIcon;
+
+document.addEventListener('DOMContentLoaded', function () {
+      const mobileNav = document.querySelector('.mobile-nav');
+  if (!mobileNav.classList.contains('open')) {
+    mobileNav.classList.add('open');
+      }
+	
   const buttons = document.querySelectorAll('.top-right-button-set button');
   const statsButtonToggle = document.querySelector('.top-right-button-set .icon-button-stats-toggle');
   const showHideAlertZonesToggle = document.querySelector('.top-right-button-set .icon-button-show-hide-alert-zones-toggle');
-  const showHideAlertZonesIcon = showHideAlertZonesToggle.querySelector('i');
+  showHideAlertZonesIcon = showHideAlertZonesToggle.querySelector('i');
 
   statsButtonToggle.addEventListener('click', () => {
     mobileNav.classList.toggle('open');
@@ -54,62 +110,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-function startRectangleDraw() {
-  console.log("startRectangleDraw()");
-  rectangleDrawer = new L.Draw.Rectangle(map);
-  currentDrawer = rectangleDrawer; // update currentDrawer
-  rectangleDrawer.enable();
-    map.on(L.Draw.Event.CREATED, function (e) {
-        let type = e.layerType;
-        let layer = e.layer;
-        
-        if (type === 'rectangle') {
-          drawnItems.addLayer(layer);
-          console.log("Finished rectangle draw", layer.getLatLngs());
-          rectangleDrawer.disable();
-          map.off(L.Draw.Event.CREATED)
-      }
-    });
-}
-	
 
-function startPolygonDraw() {
-  console.log("startPolygonDraw()");
-  polygonDrawer = new L.Draw.Polygon(map);
-  currentDrawer = polygonDrawer; // update currentDrawer
-  polygonDrawer.enable();
-    map.on(L.Draw.Event.CREATED, function (e) {
-        let type = e.layerType;
-        let layer = e.layer;
-        
-        if (type === 'polygon') {
-          drawnItems.addLayer(layer);
-          console.log("Finished polygon draw", layer.getLatLngs());
-          polygonDrawer.disable();
-          map.off(L.Draw.Event.CREATED)
-      }
-    });
-}
-		
-  // Function to make alerts visible or invisible
-  function toggleAlertVisibility(icon) {
-    const showAlertsButton = document.getElementById('showAlertsButton');
-    if (showAlertsButton.classList.contains('active')) {
-      // Show eye icon
-      icon.classList.remove('fa-eye-slash');
-      icon.classList.add('fa-eye');
-      icon.classList.add('fa-solid')
-      // Hide alerts
-      console.log("Hiding alerts");
-    } else {
-      // show eye-slash icon
-      icon.classList.remove('fa-eye');
-      icon.classList.add('fa-eye-slash');
-      icon.classList.add('fa-solid')
-      // show alerts
-      console.log("Showing alerts");
-    }
-  }
 
   function clearAllAlerts() {
     //Clear all alerts
