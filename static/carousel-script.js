eb952f2edbf9b3ecdc68a20c4eff82fdbaa38daa
simonvacher <simon@deepearth.digital>
2025-02-05 13:55:13 +0000

    Enable carousel scrolling and minor UI enhancements
    Added drag-to-scroll functionality for the date carousel, improving user interaction with horizontal scrolling. Updated CSS styles to support smooth dragging behavior and enhanced responsiveness for toggle button visibility on smaller screens.

--- static/carousel-script.js
+++ static/carousel-script.js
@@ -1,6 +1,36 @@
 const carouselItems = document.querySelectorAll('.date-carousel-item');
 const todayCarouselItem = document.getElementById('today-carousel-item');
 
+let startX;
+let scrollLeft;
+let isDown = false;
+
+const slider = document.querySelector('.date-carousel');
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return; // stop the function from running
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; // scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
 
 // Function to format a date object as "Day Date Month"
 function formatDate(date) {
